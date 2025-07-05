c8f0f0441c4fd60381ac656f0d55e5d010583a84
selvaganesh19 <selvaganesh.al22@bitsathy.ac.in>
2025-07-05 15:32:14 +0530

    Changed Layout File

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Emotion Detector in Text',
+  description: 'A simple emotion detector in text using Next.js and Python(Transformers).',
+  // generator: 'v0.dev',
 }
 
 export default function RootLayout({
